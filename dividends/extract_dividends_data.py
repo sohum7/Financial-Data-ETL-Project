e41ee4fdc356b54de3c788e7ad9250c62d1ee38f
Sohum Patel <57050073+sohum7@users.noreply.github.com>
2026-02-10 21:24:05 +0000

    Refactor dividend data extraction to encapsulate parameters within main function and add Cloud Build configuration for deployment

--- dividends/extract_dividends_data.py
+++ dividends/extract_dividends_data.py
@@ -8,9 +8,6 @@ MS_DIVIDENDS_URL = "https://api.marketstack.com/v2/dividends"
 
 MS_DIVIDENDS_DATA_BUCKET = "dividend-data-dev"
 
-# company symbols from which to extract data from
-symbols = ["AAPL", "MSFT", "GOOGL", "AMZN", "FB", "TSLA", "NVDA"]
-symbols_params_str = ",".join(symbols)
 
 def getCurWkDtRange():
     # today
@@ -21,19 +18,26 @@ def getCurWkDtRange():
     friday = monday + timedelta(days=4)              # current week Friday
     return monday, friday
 
-monday, friday = getCurWkDtRange()
-# API request to Marketstack for dividend data
-params = {
-    "access_key": MS_DIVIDENDS_API_KEY,
-    "symbols": symbols_params_str,
-    "limit": 1000,
-    "date_from": monday.strftime("%Y-%m-%d"),
-    "date_to": friday.strftime("%Y-%m-%d"),
-    "sort": "ASC"
-}
 
 
-# make the API request
+def main():
+    # company symbols from which to extract data from
+    symbols = ["AAPL", "MSFT", "GOOGL", "AMZN", "FB", "TSLA", "NVDA"]
+    symbols_params_str = ",".join(symbols)
+    
+    monday, friday = getCurWkDtRange()
+    
+    # API request to Marketstack for dividend data
+    params = {
+        "access_key": MS_DIVIDENDS_API_KEY,
+        "symbols": symbols_params_str,
+        "limit": 1000,
+        "date_from": monday.strftime("%Y-%m-%d"),
+        "date_to": friday.strftime("%Y-%m-%d"),
+        "sort": "ASC"
+    }
+    
+    # make the API request
 req = req.get(MS_DIVIDENDS_URL, params=params)
 
 # parse the response data
@@ -54,5 +58,7 @@ blob = bucket.blob(dest_blob_name)
 
 # write data to the blob
 blob.upload_from_string(json.dumps(data['data'], indent=4), content_type="application/json")
+
+
 
 pass
