67ebdf3a4e4d0b29e9e02a2ba66cc9ea3ab867f9
Sohum Patel <57050073+sohum7@users.noreply.github.com>
2026-02-11 00:19:59 +0000

    Refactor comments in main.py for clarity and consistency

--- dividends/main.py
+++ dividends/main.py
@@ -1,11 +1,8 @@
 from extract_dividends_data import main as extract_dividends_main
-#from transform_dividends_data import main as transform_main
-#from load_dividends_data import main as load_main
+#from transform_dividends_data import main as transform_dividendsmain
+#from load_dividends_data import main as load_dividends_main
 
 def extract_dividends_main(request):
     data = extract_dividends_main(request)
     print("Extraction completed")
     return "Dividends Extraction completed successfully"
-
-#transformed = transform_data(data)
-    #load_data(transformed)
