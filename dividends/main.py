986d59b637204cd7e0a72a38274963dac8812ce9
Sohum Patel <57050073+sohum7@users.noreply.github.com>
2026-02-11 00:17:27 +0000

    Rename main function to extract_main and update extraction logic in main.py

--- dividends/main.py
+++ dividends/main.py
@@ -1,9 +1,9 @@
-from extract_dividends_data import main as extract_main
+from extract_dividends_data import main as extract_dividends_main
 #from transform_dividends_data import main as transform_main
 #from load_dividends_data import main as load_main
 
-def main(request):
-    data = extract_main()
+def extract_main(request):
+    data = extract_dividends_main(request)
     print("Extraction completed")
     #transformed = transform_data(data)
     #load_data(transformed)
