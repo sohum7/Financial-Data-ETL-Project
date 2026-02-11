50df5b9e0bf516824d4887dfbc59e525fa3261f5
Sohum Patel <57050073+sohum7@users.noreply.github.com>
2026-02-11 20:28:47 +0000

    function name change for extraction process

--- dividends/main.py
+++ dividends/main.py
@@ -2,8 +2,8 @@ from extract_dividends_data import main as extract_dividends_main
 #from transform_dividends_data import main as transform_dividendsmain
 #from load_dividends_data import main as load_dividends_main
 
-def extract_dividends_main(request):
-    data = extract_dividends_main(request)
+def extract_dividends_entry_point(request):
+    extract_dividends_main(request)
     print("Extraction completed")
     print("test")
     return "Dividends Extraction completed successfully"
