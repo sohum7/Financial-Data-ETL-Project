42e54dfba0575a96c2aaebbc9ec3e483dca6fbf9
Sohum Patel <57050073+sohum7@users.noreply.github.com>
2026-02-11 00:37:20 +0000

    Add test print statement in extract_dividends_main function

--- dividends/main.py
+++ dividends/main.py
@@ -5,5 +5,5 @@ from extract_dividends_data import main as extract_dividends_main
 def extract_dividends_main(request):
     data = extract_dividends_main(request)
     print("Extraction completed")
+    print("test")
     return "Dividends Extraction completed successfully"
-pass
