0c4177188d0488b70389cbe5d7f82ee14e09b644
Sohum Patel <57050073+sohum7@users.noreply.github.com>
2026-02-11 20:38:09 +0000

    removed extract_dividends_main param

--- dividends/main.py
+++ dividends/main.py
@@ -3,7 +3,7 @@ from extract_dividends_data import main as extract_dividends_main
 #from load_dividends_data import main as load_dividends_main
 
 def extract_dividends_entry_point(request):
-    extract_dividends_main(request)
+    extract_dividends_main()
     print("Extraction completed")
     print("test")
     return "Dividends Extraction completed successfully"
