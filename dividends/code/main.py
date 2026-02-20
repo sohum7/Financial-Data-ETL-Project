b950908e2289b18c937b77881228d3f25644200e
Sohum Patel <57050073+sohum7@users.noreply.github.com>
2026-02-20 19:09:29 +0000

    refactor: restructure dividends extraction and transformation process; remove old scripts and update entry points

--- dividends/code/main.py
+++ dividends/code/main.py
@@ -1,9 +1,9 @@
-from extract_dividends_data import main as extract_dividends_main
+from dividends.code.extract import main as extract_main
 #from transform_dividends_data import main as transform_dividendsmain
 #from load_dividends_data import main as load_dividends_main
 
-def extract_dividends_entry_point(request):
-    extract_dividends_main()
+def extract_entry_point(request):
+    extract_main()
     print("Extraction completed")
     print("test")
     return "Dividends Extraction completed successfully"
