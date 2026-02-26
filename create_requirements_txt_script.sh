33539070f37b8b26a83fbb9e60db7f67d45a0f9a
Sohum Patel <57050073+sohum7@users.noreply.github.com>
2026-02-26 01:00:03 +0000

    modify requirements.txt file

---
+++ create_requirements_txt_script.sh
@@ -0,0 +1,6 @@
+cd dividends
+pipreqs --force .
+sed -i '/^protobuf==[0-9.]\+/d' requirements.txt
+pip freeze | grep google-cloud-bigquery >> requirements.txt
+pip freeze | grep google-cloud-storage >> requirements.txt
+pip freeze | grep google-cloud-logging >> requirements.txt
