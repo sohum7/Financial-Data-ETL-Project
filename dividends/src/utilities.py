4cf5f9ead5dfa1c275ee5c61fe00f8ab3d7a7cd1
Sohum Patel <57050073+sohum7@users.noreply.github.com>
2026-03-04 01:14:28 +0000

    test

--- dividends/src/utilities.py
+++ dividends/src/utilities.py
@@ -7,7 +7,7 @@ def getCurWkDtRange():
     # weekday(): Monday=0, Sunday=6
     monday = run_dt - timedelta(days=run_dt.weekday())  # current week Monday
     friday = monday + timedelta(days=4)                 # current week Friday
-    return run_dt, monday.strftime("%Y-%m-%d"), friday.strftime("%Y-%m-%d")
+    return run_dt.strftime("%Y-%m-%d"), monday.strftime("%Y-%m-%d"), friday.strftime("%Y-%m-%d")
 
 def http_return(http_code: int, msg: str= ""):
     if 200 < http_code < 299:
