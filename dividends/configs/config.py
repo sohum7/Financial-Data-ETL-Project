6527ff99b0edb7895c6a4348f41da1011e5de12c
Sohum Patel <57050073+sohum7@users.noreply.github.com>
2026-03-05 00:59:36 +0000

    refactor: update configuration handling and environment variable retrieval for Market Stack data

--- dividends/configs/config.py
+++ dividends/configs/config.py
@@ -3,18 +3,40 @@
 # This file defines constants for API keys, URLs, file formats, and GCS bucket details used across the ETL pipeline.
 
 # imports
-import os
-import configparser
+from configparser import ConfigParser
+from os import getenv as os_getenv
+from dotenv import load_dotenv
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
-DEFAULT_ENV_VAR_NAME = "APP_ENV"
-DEFAULT_ENV_VAR_VAL = "dev"
 
-env = os.getenv(DEFAULT_ENV_VAR_NAME, DEFAULT_ENV_VAR_VAL)
-config = configparser.ConfigParser()
-config.read(BASE_DIR / "config.ini") # Load base first
-config.read(BASE_DIR / f"config.{env}.ini") # Then environment override
+def load_config():
+
+    load_dotenv()
+    
+    config = ConfigParser()
+    config.read(BASE_DIR / "config.ini") # Load base first
+    
+    env_vars = {
+        "ENVIRONMENT": os_getenv("ENVIRONMENT", ""),
+        "BUCKET_SUFFIX": os_getenv("BUCKET_SUFFIX", ""),
+        "GOOGLE_CLOUD_PROJECT": os_getenv("GOOGLE_CLOUD_PROJECT", "")
+    }
+    
+    if not env_vars["GOOGLE_CLOUD_PROJECT"]:
+        raise ValueError("missing required environment variable: GOOGLE_CLOUD_PROJECT")
+    if not env_vars["ENVIRONMENT"]:
+        raise ValueError("missing required environment variable: ENV")
+    if not env_vars["BUCKET_SUFFIX"]:
+        raise ValueError("missing required environment variable: BUCKET_SUFFIX")
+    
+    return config, env_vars
+
+# Load configuration and environment variables
+config, env_vars = load_config()
+gc_project = env_vars["GOOGLE_CLOUD_PROJECT"]
+gc_env = env_vars["ENVIRONMENT"]
+gc_bucket_suffix = env_vars["BUCKET_SUFFIX"]
 
 ms_cfg = config["MARKET_STACK_METADATA"]
 ms_div_cfg = config["MARKET_STACK_DIVIDENDS_METADATA"]
@@ -27,16 +49,16 @@ MS_DIV_URL = f"{MS_BASE_URL}{MS_CAT}"
 
 ## Extract source data
 MS_DIV_RAW_FILE_TYPE = ms_div_cfg["raw_file_type"]
-MS_DIV_RAW_FILE_BUCKET_NM = ms_div_cfg["raw_file_bucket"]
+MS_DIV_RAW_FILE_BUCKET_NM = ms_div_cfg["raw_file_bucket"] + gc_bucket_suffix
 MS_DIV_RAW_FILE_BUCKET_SUBDIR = ms_div_cfg["raw_file_bucket_subdir"]
 
 ## Transformed data
 MS_DIV_TFD_FILE_TYPE = ms_div_cfg["tfd_file_type"]
-MS_DIV_TFD_FILE_BUCKET_NM = ms_div_cfg["tfd_file_bucket"]
+MS_DIV_TFD_FILE_BUCKET_NM = ms_div_cfg["tfd_file_bucket"] + gc_bucket_suffix
 MS_DIV_TFD_FILE_BUCKET_SUBDIR = ms_div_cfg["tfd_file_bucket_subdir"]
 
 ## Cleaned data
 MS_DIV_CLN_FILE_TYPE = ms_div_cfg["cln_file_type"]
-MS_DIV_CLN_FILE_BUCKET_NM = ms_div_cfg["cln_file_bucket"]
+MS_DIV_CLN_FILE_BUCKET_NM = ms_div_cfg["cln_file_bucket"] + gc_bucket_suffix
 MS_DIV_CLN_FILE_BUCKET_SUBDIR = ms_div_cfg["cln_file_bucket_subdir"]
 
