4d7c6a9f5329741b1bb93c81d56d09742d676155
Sohum Patel <57050073+sohum7@users.noreply.github.com>
2026-03-06 22:54:43 +0000

    test

--- dividends/extract/main.py
+++ dividends/extract/main.py
@@ -3,7 +3,7 @@
 # Local imports
 from extract.src.cloud_function_job import extract, extract_dividends
 
-def cloud_function_entry_point(request):
+def extract_entry_point(request):
     return extract_dividends(request)
 
 if __name__ == "__main__":
